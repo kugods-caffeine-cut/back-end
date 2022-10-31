eaf3b4d2739ff45049d1b838437d402fb18757fd
KY00KIM <hived@naver.com>
2022-10-31 17:04:12 +0900

    [ADD] add boilerplate

---
+++ config/mongoose.js
@@ -0,0 +1,28 @@
+
+/*
+    Mongoose connect example
+    FROM: https://mongoosejs.com/
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
+*/
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const connectMongoDB = () => {
+    mongoose.connect(
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_NAME}.fhokkc8.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+            console.log("DB CONNECTION : SUCCESS")
+        }, (err) => { console.log("DB CONNECTION : ERROR\nERROR : " + err) })
+
+}
+
+module.exports = {
+    connectMongoDB
+}
