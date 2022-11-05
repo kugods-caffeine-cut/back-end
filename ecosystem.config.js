b4b7ae986a89ed442b20ddd6f500c0626a9c7aeb
KyuminKim <hivedora@gmail.com>
2022-11-05 12:37:35 +0900

    add: server test code

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [{
+        name: 'caffeine_server',
+        script: './index.js',
+        instances: 0,
+        exec_mode: 'cluster',
+        wait_ready: true,
+        listen_timeout: 50000,
+        kill_timeout: 5000,
+        watch: ["server", "client"],
+        watch_delay: 2000,
+        time: true,
+    }]
+}
--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get('/test', (req, res) => {
+    res.send('Hello World! TEST')
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+}
+
+
+
+)
