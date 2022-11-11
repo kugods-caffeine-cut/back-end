5896a94736c2f08b061b86a2e7dabe9b221a4e7f
Baekgyu <centneuf0109@gmail.com>
2022-11-11 23:39:33 +0900

    [FTR] postDrink

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -8,7 +8,17 @@ export const getAllDrink = async (req, res) => {
   res.send(drinksInfo);
 };
 export const postDrink = async (req, res) => {
-  res.send("postDrink 작업중");
+  const {brand, drink_name, temp, img, size, kcal, caffeine} = req.body;
+  const newDrink = await Drink.create({
+    brand,
+    drink_name,
+    temp,
+    img,
+    size,
+    kcal,
+    caffeine,
+  });
+  res.send(newDrink);
 };
 export const getOneDrink = async (req, res) => {
   const drinkID = req.params;
