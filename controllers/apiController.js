6bd6dc06cbc12c008be363961d2023e8e38cc01d
Baekgyu <centneuf0109@gmail.com>
2022-11-11 23:02:01 +0900

    [FTR] getOneDrink

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -11,7 +11,9 @@ export const postDrink = async (req, res) => {
   res.send("postDrink 작업중");
 };
 export const getOneDrink = async (req, res) => {
-  res.send("getOneDrink 작업중");
+  const drinkID = req.params;
+  const drinkInfo = await Drink.findById(drinkID);
+  res.send(drinkInfo);
 };
 export const patchDrink = async (req, res) => {
   res.send("patchDrink 작업중");
