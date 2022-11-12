0ce6fa0978ed1fb1417747521ff5492da1a16325
Baekgyu <centneuf0109@gmail.com>
2022-11-12 23:12:49 +0900

    [FTR] patchDrink

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -25,3 +25,21 @@ export const getOneDrink = async (req, res) => {
   const drinkInfo = await Drink.findById(drinkID);
   res.send(drinkInfo);
 };
+export const patchDrink = async (req, res) => {
+  const {drinkID} = req.params;
+  const {brand, drink_name, temp, img, size, kcal, caffeine} = req.body;
+  const newDrink = await Drink.findByIdAndUpdate(
+    drinkID,
+    {
+      brand,
+      drink_name,
+      temp,
+      img,
+      size,
+      kcal,
+      caffeine,
+    },
+    {new: true},
+  );
+  res.send(newDrink);
+};
