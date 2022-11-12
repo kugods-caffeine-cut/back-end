65f2a6213891335e6103b249e2c908b447f5cd0e
Baekgyu <centneuf0109@gmail.com>
2022-11-12 15:45:18 +0900

    [DOC] mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
-mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_NAME}.awwmmyt.mongodb.net/?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-  },
-);
+const handleDBConnect = () => {
+  console.log("✅ Connected to DB");
+};
+const handleDBError = error => {
+  console.log("❌ DB Error", error);
+};
 
-const connectMongoDB = mongoose.connection;
-
-const handleOpen = () => console.log("✅ Connected to DB");
-const handleError = error => console.log("❌ DB Error", error);
-
-connectMongoDB.on("error", handleError);
-connectMongoDB.once("open", handleOpen);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_NAME}.awwmmyt.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+    },
+  )
+  .then(handleDBConnect, handleDBError);
