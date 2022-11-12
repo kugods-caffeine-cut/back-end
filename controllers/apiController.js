e181ddb9cdde548d3f2f194f309b951107ce79f9
Baekgyu <centneuf0109@gmail.com>
2022-11-12 23:12:16 +0900

    [FTR] getAllDrink

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,3 +3,7 @@ import Drink from "../model/Drink";
 export const getApi = async (req, res) => {
   res.send("API를 위한 router, controller 입니다.");
 };
+export const getAllDrink = async (req, res) => {
+  const drinksInfo = await Drink.find();
+  res.send(drinksInfo);
+};
