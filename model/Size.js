c7c22413a72a64a41b876d57ce1d52b7b11a653a
Baekgyu <centneuf0109@gmail.com>
2022-11-12 23:11:25 +0900

    [DOC] Size.js

--- model/Size.js
+++ model/Size.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
-const sizeSchema = new mongoose.Schema({
-  shopName: {type: String},
-  numSize: {type: Number},
-  strSize: {type: String},
-});
+const sizeSchema = new mongoose.Schema(
+  {
+    brand: {type: String},
+    size_onze: {type: mongoose.Schema.Types.Mixed},
+    // 2차원 배열입니다. (2차원 배열을 몽구스로 정의할 수 없어서 Mixed타입으로 정의함)
+    //예) [["숏", 8], ["톨", 12] ... ]
+  },
+  {timestamps: true},
+);
 
 const Size = mongoose.model("Size", sizeSchema);
 export default Size;
