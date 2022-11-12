e402b868801eee8464aa43ca3f1e60d4a3b9da33
Baekgyu <centneuf0109@gmail.com>
2022-11-12 23:23:11 +0900

    [DOC] mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,34 +1,17 @@
-/*
-    Mongoose connect example
-    FROM: https://mongoosejs.com/
+import mongoose from "mongoose";
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test');
-
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
-
-*/
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const connectMongoDB = () => {
-  mongoose
-    .connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_NAME}.awwmmyt.mongodb.net/?retryWrites=true&w=majority`,
-    )
-    .then(
-      () => {
-        console.log("DB CONNECTION : SUCCESS");
-      },
-      err => {
-        console.log("DB CONNECTION : ERROR\nERROR : " + err);
-      },
-    );
+const handleDBConnect = () => {
+  console.log("✅ Connected to DB");
+};
+const handleDBError = error => {
+  console.log("❌ DB Error", error);
 };
 
-module.exports = {
-  connectMongoDB,
-};
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_NAME}.awwmmyt.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+    },
+  )
+  .then(handleDBConnect, handleDBError);
