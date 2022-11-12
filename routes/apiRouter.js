9225e70ed195761ec13c6dcc8a4ce86f646655fb
Baekgyu <centneuf0109@gmail.com>
2022-11-12 23:30:03 +0900

    [DOC] apiRouter.js

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,3 +1,20 @@
 import express from "express";
+import {
+  getApi,
+  getAllDrink,
+  postDrink,
+  getOneDrink,
+  patchDrink,
+  deleteDrink,
+} from "../controllers/apiController";
+
 const apiRouter = express.Router();
+
+apiRouter.route("/drink").get(getAllDrink).post(postDrink);
+apiRouter
+  .route("/drink/:drinkID")
+  .get(getOneDrink)
+  .patch(patchDrink)
+  .delete(deleteDrink);
+
 export default apiRouter;
