5357dbc3d4ea7484f498ddd76496bef3a636ac3b
Baekgyu <centneuf0109@gmail.com>
2022-11-13 14:37:55 +0900

    [DOC] Fix Typo

--- model/Size.js
+++ model/Size.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const sizeSchema = new mongoose.Schema(
   {
     brand: {type: String},
-    size_onze: {type: mongoose.Schema.Types.Mixed},
+    size_oz: {type: mongoose.Schema.Types.Mixed},
     // 2차원 배열입니다. (2차원 배열을 몽구스로 정의할 수 없어서 Mixed타입으로 정의함)
     //예) [["숏", 8], ["톨", 12] ... ]
   },
