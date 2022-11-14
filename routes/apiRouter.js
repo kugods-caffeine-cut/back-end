296e0011603d02ba3518527fb3b532ae72590fd9
KyuminKim <hivedora@gmail.com>
2022-11-14 20:47:34 +0900

    mod : disable del/patch-drink-api

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,8 +4,8 @@ const DrinkController = require("../controllers/apiController");
 apiRouter.get("/drink", DrinkController.getAllDrink);
 apiRouter.post("/drink", DrinkController.postDrink);
 apiRouter.get("/drink/:drinkID", DrinkController.getOneDrink);
-apiRouter.patch("/drink/:drinkID", DrinkController.patchDrink);
-apiRouter.delete("/drink/:drinkID", DrinkController.deleteDrink);
+//apiRouter.patch("/drink/:drinkID", DrinkController.patchDrink);
+//apiRouter.delete("/drink/:drinkID", DrinkController.deleteDrink);
 apiRouter.get("/drink/search/:searchKeyword", DrinkController.searchDrink);
 
 module.exports = {apiRouter};
