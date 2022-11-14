8079830fbc6d951e86657a8e016d14806d2f498d
KyuminKim <hivedora@gmail.com>
2022-11-14 23:46:15 +0900

    add : swagger-document

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const rootRouter = require("express").Router();
 const RootController = require("../controllers/rootController");
 const {apiRouter} = require("./apiRouter");
+const {swaggerUi, specs} = require("../config/swagger");
 
 rootRouter.get("/", RootController.getHome);
 rootRouter.use("/api", apiRouter);
+rootRouter.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 module.exports = {rootRouter};
