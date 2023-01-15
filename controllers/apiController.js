f2c1b343c3d002148201e965ff1a39699abfb161
Baekgyu <centneuf0109@gmail.com>
2023-01-15 15:02:02 +0900

    doc: controllers not used

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -66,42 +66,42 @@ const DrinkController = {
     }
   },
 
-  patchDrink: async (req, res) => {
-    try {
-      const {drinkID} = req.params;
-      const {brand, drink_name, temp, img, size, kcal, caffeine} = req.body;
-      const newDrink = await Drink.findByIdAndUpdate(
-        drinkID,
-        {
-          brand,
-          drink_name,
-          temp,
-          img,
-          size,
-          kcal,
-          caffeine,
-        },
-        {new: true},
-      );
-      httpResponse.SUCCESS_OK(res, "", newDrink);
-    } catch (error) {
-      httpResponse.BAD_REQUEST(res, "", error);
-    }
-  },
+  // patchDrink: async (req, res) => {
+  //   try {
+  //     const {drinkID} = req.params;
+  //     const {brand, drink_name, temp, img, size, kcal, caffeine} = req.body;
+  //     const newDrink = await Drink.findByIdAndUpdate(
+  //       drinkID,
+  //       {
+  //         brand,
+  //         drink_name,
+  //         temp,
+  //         img,
+  //         size,
+  //         kcal,
+  //         caffeine,
+  //       },
+  //       {new: true},
+  //     );
+  //     httpResponse.SUCCESS_OK(res, "", newDrink);
+  //   } catch (error) {
+  //     httpResponse.BAD_REQUEST(res, "", error);
+  //   }
+  // },
 
-  deleteDrink: async (req, res) => {
-    try {
-      const {drinkID} = req.params;
-      await Drink.findByIdAndDelete(drinkID);
-      httpResponse.SUCCESS_OK(
-        res,
-        `id가 ${drinkID}인 drink를 삭제했습니다.`,
-        {},
-      );
-    } catch (error) {
-      httpResponse.BAD_REQUEST(res, "", error);
-    }
-  },
+  // deleteDrink: async (req, res) => {
+  //   try {
+  //     const {drinkID} = req.params;
+  //     await Drink.findByIdAndDelete(drinkID);
+  //     httpResponse.SUCCESS_OK(
+  //       res,
+  //       `id가 ${drinkID}인 drink를 삭제했습니다.`,
+  //       {},
+  //     );
+  //   } catch (error) {
+  //     httpResponse.BAD_REQUEST(res, "", error);
+  //   }
+  // },
 
   searchDrink: async (req, res) => {
     try {
