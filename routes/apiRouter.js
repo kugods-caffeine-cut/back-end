5b548ffd2e6374547b670357bdd96f8b947aa39f
Baekgyu <centneuf0109@gmail.com>
2023-01-15 15:00:34 +0900

    doc: apiRouter.js

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,11 +1,16 @@
 const apiRouter = require("express").Router();
 const DrinkController = require("../controllers/apiController");
 
-apiRouter.get("/drink", DrinkController.getAllDrink);
-apiRouter.post("/drink", DrinkController.postDrink);
+apiRouter.get("/drink", DrinkController.getAllDrinks);
 apiRouter.get("/drink/:drinkID", DrinkController.getOneDrink);
+apiRouter.post("/drink", DrinkController.postDrink);
 //apiRouter.patch("/drink/:drinkID", DrinkController.patchDrink);
 //apiRouter.delete("/drink/:drinkID", DrinkController.deleteDrink);
-apiRouter.get("/drink/search/:searchKeyword", DrinkController.searchDrink);
+apiRouter.get("/drink/search/:searchKeyword", DrinkController.getSearchDrink);
+
+apiRouter.get("/log", DrinkController.getAllLogs);
+apiRouter.get("/log/:logID", DrinkController.getOneLog);
+apiRouter.post("/log", DrinkController.postLog);
+apiRouter.get("/favorite/:userID", DrinkController.getFavoriteDrinks);
 
 module.exports = {apiRouter};
