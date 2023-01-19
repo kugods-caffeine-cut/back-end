acfdd47774ef2aec82cff66bcf956d65552f1cfb
KyuminKim <hivedora@gmail.com>
2023-01-19 16:14:24 +0900

    fix: merge-typo

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const RootController = require("../controllers/rootController");
 const {apiRouter} = require("./apiRouter");
 const {swaggerUi, specs} = require("../config/swagger");
 
-rootRouter.get("/", RootController.getHome);
-rootRouter.use("/api", apiRouter);
-rootRouter.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+RootRouter.get("/", RootController.getHome);
+RootRouter.use("/api", apiRouter);
+RootRouter.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 module.exports = {RootRouter};
