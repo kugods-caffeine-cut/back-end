7a75e44357060ed8e09a8ef51574b8ffcd8b2371
KyuminKim <hivedora@gmail.com>
2023-02-16 18:47:19 +0900

    ftr: api authenication

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,10 @@ const jwt = require("../lib/jwt");
 
 const AuthController = {
   async signIn(req, res) {
-    const {id} = req.body;
+    const {kakaoId} = req.body;
 
     try {
-      const user = await User.find({_id: id, isDeleted: false});
+      const user = await User.findOne({kakaoId: kakaoId, isDeleted: false});
 
       if (user) {
         const token = jwt.sign(user);
@@ -17,6 +17,25 @@ const AuthController = {
       return httpResponse.UNAUTHORIZED(res);
     } catch (e) {
       return httpResponse.INTERNAL_ERROR(res, "", e);
+    }
+  },
+  async checkToken(req, res, next) {
+    try {
+      if (
+        req.originalUrl == "/api/auth/signUp" ||
+        req.originalUrl == "/api/auth/signIn"
+      ) {
+        return next();
+      } else {
+        const token = req.headers.authorization.split("Bearer ")[1];
+        const authValid = jwt.verify(token);
+        if (authValid.success) {
+          return next();
+        }
+        return httpResponse.UNAUTHORIZED(res, "INVALID ACCESS_TOKEN", {});
+      }
+    } catch (error) {
+      return httpResponse.BAD_REQUEST(res, "", {error});
     }
   },
 };
